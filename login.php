d62b6f3801f2a97edaf837651c5c2096586a2a0a
s3677729 <s3677729@student.rmit.edu.au>
2021-05-25 00:01:02 +1000

    Added logout, register, adding dynamodb connection into login.php, still not deployed into EB

--- login.php
+++ login.php
@@ -1,19 +1,75 @@
+<?php
+
+  session_start();
+
+
+  require "vendor/autoload.php";
+
+  use Aws\DynamoDb\Exception\DynamoDbException;
+  use Aws\DynamoDb\Marshaler;
+  date_default_timezone_set('UTC');
+  
+  session_start();
+  
+  if ( isset( $_SESSION["name"] ) ) {
+    header("Location: main.php");
+  }
+  
+  $sdk = new Aws\Sdk([
+    'region' => 'us-east-1',
+    'version' => 'latest'
+  ]);
+  
+  if ( isset( $_POST["submit"] ) ) {
+    if ( empty( $_POST["username"] ) || empty( $_POST["password"] ) ) {
+      echo '<script language="javascript">';
+      echo 'alert("Username or password can\'t be empty.")';
+      echo '</script>';
+    } else {
+      $dynamodb = $sdk->createDynamoDb();
+      $marshaler = new Marshaler();
+  
+      $tableName = 'users-dev';
+  
+          $username = $_POST["username"];
+          $password = $_POST["password"];
+  
+      $params = [
+        'TableName' => $tableName
+      ];
+  
+      $results = $dynamodb->scan($params);
+  
+      foreach( $results['Items'] as $item ) {
+        $usernameItem = $marshaler->unmarshalValue($item['username']);
+        $passwordItem = $marshaler->unmarshalValue($item['password']);
+        if ( $username == $usernameItem  and $password == $passwordItem) {
+          $_SESSION["name"] = $marshaler->unmarshalValue($item['user_name']);
+          header("Location: main.php");
+        }
+      }
+      echo '<script language="javascript">';
+      echo 'alert("Username or password is invalid.")';
+      echo '</script>';
+  
+    }
+  }
+  
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.83.1">
-    <title>Signin Template · Bootstrap v5.0</title>
+    <title>Login Page</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
 
     
 
     <!-- Bootstrap core CSS -->
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
       .bd-placeholder-img {
@@ -33,31 +89,54 @@
 
     
     <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
+    <link href="css/signin.css" rel="stylesheet">
+    <link href="css/navbar-top-fixed.css" rel="stylesheet">
   </head>
   <body class="text-center">
     
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Event</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <a class="nav-link" href="main.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="register.php">Register</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
 <main class="form-signin">
-  <form>
-    <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+  <form action="login.php" method="POST">
+    <img class="mb-4" src="assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Login Form</h1>
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
+      <input type="email" class="form-control" id="floatingInput" placeholder="JohnDoe" name="username">
+      <label for="floatingInput">Username</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
       <label for="floatingPassword">Password</label>
     </div>
 
-    <div class="checkbox mb-3">
+    <div class="mb-3">
       <label>
-        <input type="checkbox" value="remember-me"> Remember me
+        Not registered? <a href="register.php">Register</a>.
       </label>
     </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+    <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Log In</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2021–2021</p>
   </form>
 </main>
 
--- main.php
+++ main.php
@@ -3,17 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.83.1">
-    <title>Fixed top navbar example · Bootstrap v5.0</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/navbar-fixed/">
+    <title>Main Page</title>
 
     
 
     <!-- Bootstrap core CSS -->
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
       .bd-placeholder-img {
@@ -39,26 +35,22 @@
     
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Fixed navbar</a>
+    <a class="navbar-brand" href="#">Event</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="main.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="login.php">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="register.php">Register</a>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
@@ -67,12 +59,12 @@
   <div class="bg-light p-5 rounded">
     <h1>Navbar example</h1>
     <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
-    <a class="btn btn-lg btn-primary" href="../components/navbar/" role="button">View navbar docs &raquo;</a>
+    <a class="btn btn-lg btn-primary" href="components/navbar/" role="button">View navbar docs &raquo;</a>
   </div>
 </main>
 
 
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 
       
   </body>
